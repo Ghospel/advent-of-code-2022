188fed2980bed5ace51eb277d4d0d5c877e63380
Jasper Huizing <jasper.huizing@wearetriple.com>
2023-11-29 20:16:29 +0100

    day 1 part 1

---
+++ Advent of code 2022/Day1/Program.cs
@@ -0,0 +1,25 @@
+﻿
+var input = await File.ReadAllLinesAsync("input.txt");
+
+int highest = 0;
+int current = 0;
+
+foreach(var line in input)
+{
+
+    if (string.IsNullOrWhiteSpace(line))
+    {
+        if(current > highest)
+        {
+            highest = current;
+        }
+        current = 0;
+    } else
+    {
+        current += int.Parse(line);
+    }
+}
+
+Console.WriteLine($"Highest is: {highest}");
+
+Console.ReadKey();
