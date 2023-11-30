8d382a3133670fcb12fc1d7bf0216703ebd6999d
Jasper Huizing <jasper.huizing@wearetriple.com>
2023-11-30 08:21:33 +0100

    day 2 part 2

--- Advent of code 2022/Day2/Program.cs
+++ Advent of code 2022/Day2/Program.cs
@@ -7,11 +7,29 @@ var rounds = new List<Round>();
 foreach (var line in input)
 {
     var chars = line.Split(' ');
+    var opponentHand = FromInput(chars[0]);
 
     Round round = new()
     {
-        OpponentHand = FromInput(chars[0]),
-        MyHand = FromInput(chars[1]),
+        OpponentHand = opponentHand,
+
+        // X for lose, Y for draw, Z for win
+        MyHand = chars[1] switch
+        {
+            "X" when opponentHand is Hand.Scissors => Hand.Paper,
+            "X" when opponentHand is Hand.Rock => Hand.Scissors,
+            "X" when opponentHand is Hand.Paper => Hand.Rock,
+
+            "Y" when opponentHand is Hand.Scissors => Hand.Scissors,
+            "Y" when opponentHand is Hand.Rock => Hand.Rock,
+            "Y" when opponentHand is Hand.Paper => Hand.Paper,
+
+            "Z" when opponentHand is Hand.Scissors => Hand.Rock,
+            "Z" when opponentHand is Hand.Rock => Hand.Paper,
+            "Z" when opponentHand is Hand.Paper => Hand.Scissors,
+
+            _ => throw new ArgumentOutOfRangeException()
+        }
     };
 
     rounds.Add(round);
@@ -26,15 +44,15 @@ Console.ReadKey();
 static Hand FromInput(string input)
 {
     // A for rock, B for paper, C for scissors
-    // X for rock, Y for paper, Z for scissors
+
 
     input = input.Trim();
 
     return input switch
     {
-        "A" or "X" => Hand.Rock,
-        "B" or "Y" => Hand.Paper,
-        "C" or "Z" => Hand.Scissors,
+        "A" => Hand.Rock,
+        "B" => Hand.Paper,
+        "C" => Hand.Scissors,
         _ => throw new ArgumentOutOfRangeException()
     };
 }
