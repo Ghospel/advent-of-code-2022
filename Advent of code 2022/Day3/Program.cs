54e5ff42ad0260f49eb730f87a29da4ad74ff015
Jasper Huizing <jasper.huizing@wearetriple.com>
2023-11-30 18:27:54 +0100

    day 3 part 1

---
+++ Advent of code 2022/Day3/Program.cs
@@ -0,0 +1,28 @@
+﻿var input = await File.ReadAllLinesAsync("input.txt");
+
+var items = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+var prioritySum = 0;
+
+foreach (var line in input)
+{
+    var firstHalf = line[..(line.Length/2)];
+    var secondHalf = line[(line.Length / 2)..];
+
+    foreach(var item in firstHalf)
+    {
+        bool hasFoundMatch = false;
+
+        // it also exists in de other compartment.
+        if(secondHalf.IndexOf(item) != -1)
+        {
+            prioritySum += items.IndexOf(item) + 1;
+            hasFoundMatch = true;
+        }
+
+        if (hasFoundMatch) break;
+    }
+}
+
+
+Console.WriteLine($"Sum of prios: {prioritySum}");
